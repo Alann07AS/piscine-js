0a2f55c2487a4b9e45bc26e36b4cefe5a209814e
Alann <SchnebelenAlann@hotmail.com>
2022-10-24 18:04:00 +0200

    dsqd

---
+++ method-man.js
@@ -0,0 +1,16 @@
+function words(params) {
+    return params.split(' ')
+}
+function sentence(params) {
+    return params.join(' ')
+}
+function yell(params) {
+    return params.toUpperCase()
+}
+function whisper(params) {
+    return '*' + params.toLowerCase() + '*'
+}
+function capitalize(params) {
+    params[0].toUpperCase()
+    return params
+}
