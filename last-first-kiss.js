5affbd80c215e7cb56eb49a25b7c711fd775861f
Alann <SchnebelenAlann@hotmail.com>
2022-10-24 16:12:08 +0200

    sdkfdsj

---
+++ last-first-kiss.js
@@ -0,0 +1,9 @@
+function first(params) {
+    return params[0]
+}
+function last(params) {
+    return params[params.lengt-1]
+}
+function kiss(params) {
+    return [last(params), first(params)]
+}
