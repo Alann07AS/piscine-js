1179aa5fac9c8d08235b3c33fea231de134747b5
Alann <SchnebelenAlann@hotmail.com>
2022-10-24 22:23:52 +0200

    dfsdf

--- is.js
+++ is.js
@@ -1,85 +1,14 @@
-
+const is = {}
 // remove before push !!
-
-is.num = function (params) {
-    if (typeof(params) === 'number') {
-        return true
-    } else {
-        return false
-    }
-}
-is.nan = function (params) {
-    if (isNaN(params)) {
-        return true
-    } else {
-        return false
-    }
-
-}
-is.str = function (params) {
-    if (typeof(params) === 'string') {
-        return true
-    } else {
-        return false
-    }
-
-}
-is.bool = function (params) {
-    if (typeof(params) === 'boolean') {
-        return true
-    } else {
-        return false
-    }
-}
-is.undef = function (params) {
-    if (typeof(params) === 'undefined') {
-        return true
-    } else {
-        return false
-    }
-}
-is.def = function (params) {
-    if (typeof(params) === 'undefined') {
-        return false
-    } else {
-        return true
-    }
-}
-is.arr = function (params) {
-    if (params.isAray()) {
-        return true
-    } else {
-        return false
-    }
-}
-is.obj = function (params) {
-    if (typeof(params) === 'object') {
-        return true
-    } else {
-        return false
-    }
-}
-is.fun = function (params) {
-    if (typeof(params) === 'function') {
-        return true
-    } else {
-        return false
-    }
-}
-is.truthy = function (params) {
-    if ((params)) {
-        return true
-    } else {
-        return false
-    }
-}
-is.falsy = function (params) {
-    if (!(params)) {
-        return true
-    } else {
-        return false
-    }
-}
-
+is.num = (n) => typeof(n) === 'number'
+is.nan = (n) => Number.isNaN(n)
+is.str = (n) => typeof(n) === 'string'
+is.bool = (n) => typeof(n) === 'boolean'
+is.undef = (n) => typeof(n) === 'undefined'
+is.def = (n) => !(typeof(n) === 'undefined')
+is.arr = (n) => Array.isArray(n)
+is.obj = (n) => typeof n === 'object' && !is.fun(n) && !is.arr(n) && n !== null
+is.fun = (n) => typeof(n) === 'function'
+is.truthy = (n) => !!n
+is.falsy = (n) => !n
 // !!!!!!!!
-
