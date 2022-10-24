c40a1da083fad35644f093327995d2c45d1e8f23
Alann <SchnebelenAlann@hotmail.com>
2022-10-24 16:35:44 +0200

    fdsf

---
+++ change.js
@@ -0,0 +1,12 @@
+// const sourceObject = {
+//     num: 42,
+//     bool: true,
+//     str: 'some text',
+//     log: console.log,
+// }
+
+
+function get(params) {
+    return sourceObject[params]
+}
+// console.log(get('str'))
