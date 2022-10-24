ede0cbab70ae49213ba01fcf2c4a9cc7f5fd1552
Alann <SchnebelenAlann@hotmail.com>
2022-10-24 17:01:22 +0200

    qsfdsj

---
+++ dog-years.js
@@ -0,0 +1,19 @@
+orbitalPlanet = {
+    earth : 1.0,
+    mercury : 0.2408467,
+    venus : 0.61519726,
+    mars : 1.8808158,
+    jupiter : 11.862615,
+    saturn : 29.447498,
+    uranus : 84.016846,
+    neptune : 164.79132
+}
+
+oneEarthYearSecond = 31557600
+
+function dogYears(planet, age) {
+    ageYear = (age / oneEarthYearSecond)*7
+    newAge = ageYear * orbitalPlanet[planet]
+    return Math.round(newAge * 100) / 100
+
+}
