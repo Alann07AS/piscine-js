78d5c884d0584e561bb9608485a01bc2cba0a6b8
Alann <SchnebelenAlann@hotmail.com>
2022-10-24 15:15:53 +0200

    first mute

---
+++ mutability.js
@@ -0,0 +1,17 @@
+const person = {
+    name: 'Rick',
+    age: 77,
+    country: 'US',
+}
+
+const clone1 = {
+    name: person.name,
+    age: person.age,
+    country: person.country,
+}
+const clone2 = {
+    name: person.name,
+    age: person.age,
+    country: person.country,
+}
+const samePerson = person
