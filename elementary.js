198a09ba02d13181ab8127f3465755009dfd8729
Alann <SchnebelenAlann@hotmail.com>
2022-10-26 15:00:41 +0200

    sdfds

--- elementary.js
+++ elementary.js
@@ -1,16 +1,30 @@
 function multiply(a, b) {
+    let s = 1
+    if (a >= 0) {
+        s = -1
+    }
     let r = b
-    for (;a !== 1; a--) {
+    for (;a !== 0; a+=s) {
         b += r
+    }
+    b -=r
+    if (s === 1) {
+        return -b
     }
     return b
 }
 
 function divide(a, b) {
-    let i = 1
+    let neg = false
+    if (a < 0) { a = -a; neg = true }
+    if (b < 0 && neg === true) { b = -b; neg = false } else if (b < 0) { b = -b; neg = true}
+    let i = 0
     if (a>b) { let t = a; a = b; b = t}
-    for (;multiply(i, a) < b; i++) {}
+    for (;multiply(i, a) <= b; i++) {}
     i--
+    if (neg) {
+        return -i
+    }
     return i
 }
 
@@ -18,4 +32,4 @@ function modulo(a, b) {
     return a-multiply(divide(a, b),b)
 }
 
-// console.log(modulo(119, 20))
+console.log(modulo(-22,10))
