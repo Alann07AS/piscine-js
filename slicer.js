8cc95a85989e609b46592b8e0b09142501b064d5
Alann <SchnebelenAlann@hotmail.com>
2022-10-26 16:20:38 +0200

    dfsf

--- slicer.js
+++ slicer.js
@@ -1,7 +1,7 @@
 function slice(sli, i = 0, imax = Array(...sli).length) {
     if (i < 0 || i > sli.length) {i = 0; imax = Array(...sli).length}
     let a = []
-    console.log(i, imax)
+    // console.log(i, imax)
     for (;i < imax; i++) {
         a.push(sli[i])
     }
