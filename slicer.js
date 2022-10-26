f1cecfac59a9914f4abc52e154c80413b9116559
Alann <SchnebelenAlann@hotmail.com>
2022-10-26 16:08:09 +0200

    dfsf

--- slicer.js
+++ slicer.js
@@ -1,3 +1,12 @@
-function slice(params) {
-    
+function slice(sli, i = 0, imax = Array(...sli).length) {
+    let a = []
+    console.log(i, imax)
+    for (;i < imax; i++) {
+        a.push(sli[i])
+    }
+    return a
 }
+
+
+// const a = [2,8,9]
+// console.log(slice(a,2))
