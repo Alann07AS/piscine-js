1ec2a9274f7eec19933d3af5e1632aa48dc327d8
Alann <SchnebelenAlann@hotmail.com>
2022-10-27 14:42:05 +0200

    sqdqsd

--- unbreakable.js
+++ unbreakable.js
@@ -11,16 +11,17 @@ function split(str, sep) {
     let firstPose = 0
     for (let i = 0; i < str.length; i++) {
         let last = i
+        if (i+sep.length-1 > str.length-1) {break}
         if (str.slice(i,i+sep.length) == sep ) {
             newArr.push(str.slice(firstPose,i))
             firstPose = i+sep.length
-            i = i+sep.length
         };
         if (i >= str.length-1) {
+            console.log(str.slice(firstPose,str.length))
             newArr.push(str.slice(firstPose,str.length))
         }
     }
     return newArr
 }
 
-console.log(split('ee,ff,g,', ','))
+// console.log(split('rrrr','rr'))
