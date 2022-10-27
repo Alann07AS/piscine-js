b27962347199e53ac6308dcdd266acaf3babf4c9
Alann <SchnebelenAlann@hotmail.com>
2022-10-27 17:17:49 +0200

    qsdsqd

--- get.js
+++ get.js
@@ -3,12 +3,8 @@ function get(src, path) {
     if (allPath.length == 1) {return src[path]}
     const recuGet = function (src, path, i) {
         if (i == path.length) {return src}
+        if (src[path[i]] == undefined) {return undefined}
         return recuGet(src[path[i]], path, i+1)
     }
     return recuGet(src, allPath, 0)
 }
-
-// const sr = { nested: { key: 'peekaboo' } }
-// const path = 'nested.key'
-// 
-// console.log(get(sr, path) )// -> 'peekaboo'
