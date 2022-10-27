41564c3b464f3b936e17dab601b44bfdcdfba4c0
Alann <SchnebelenAlann@hotmail.com>
2022-10-27 10:13:55 +0200

    dqsdds

--- keep-cut.js
+++ keep-cut.js
@@ -1,21 +1,26 @@
 function cutFirst (str) {
+    if (str.length <= 2) {return str}
     return str.slice(2)
 }
 function cutLast (str) {
     return str.slice(0, str.length-2)
 }
 function cutFirstLast (str) {
+    if (str.length <= 2) {return str}
     return  cutLast(cutFirst(str))
 }
 function keepFirst (str) {
+    if (str.length <= 2) {return str}
     return str.slice(0, 2)
 }
 function keepLast (str) {
+    if (str.length <= 2) {return str}
     return str.slice(str.length-2, str.length)
 }
 function keepFirstLast (str) {
+    if (str.length <= 2) {return str}
     return keepFirst(str)+keepLast(str)
 }
 
-// let t = 'azdsgf'
-// console.log(keepFirstLast(t))
+// let t = 'ab'
+// console.log(cutFirst(t))
