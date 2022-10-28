fa11f2386edc4a54cb6ef16b64db691575505da6
Alann <SchnebelenAlann@hotmail.com>
2022-10-28 10:59:12 +0200

    sdqfsdf

--- find-expression.js
+++ find-expression.js
@@ -15,29 +15,29 @@ function findExpression(nb) {
         for (let i = (nb-temp)/4; i > 0 ; i--) {
             log.push(add4); temp += 4
         }
-        return '1 ' + log.join('')
+        return '1 ' + log.join(' ')
     }
     if ((nb-1)%4 == 0) {
         for (;temp != nb;) {
             log.push(add4); temp += 4
         }
-        return '1 ' + log.join('')
+        return '1 ' + log.join(' ')
     }
     return undefined
 }
 
-console.log((findExpression(9)),'   ', 8) // 8))
-console.log((findExpression(14)),'   ', 14) // 14))
-console.log((findExpression(60)),'   ', 60) // 60))
-console.log((findExpression(100)),'   ', 100) // 100))
-console.log((findExpression(100)),'   ', 100) // 100))
-console.log((findExpression(280)),'   ', 280) // 280))
-console.log((findExpression(110)),'   ', 110) // 110))
-console.log((findExpression(144)),'   ', 144) // 144))
-console.log((findExpression(200)),'   ', 200) // 200))
-console.log((findExpression(104)),'   ', 104) // 104))
-
-console.log(findExpression(7)) // undefined))
-console.log(findExpression(63)) // undefined))
-console.log(findExpression(23)) // undefined))
-console.log(findExpression(103)) // undefined))
+// console.log((findExpression(9)),'   ', 8) // 8))
+// console.log((findExpression(14)),'   ', 14) // 14))
+// console.log((findExpression(60)),'   ', 60) // 60))
+// console.log((findExpression(100)),'   ', 100) // 100))
+// console.log((findExpression(100)),'   ', 100) // 100))
+// console.log((findExpression(280)),'   ', 280) // 280))
+// console.log((findExpression(110)),'   ', 110) // 110))
+// console.log((findExpression(144)),'   ', 144) // 144))
+// console.log((findExpression(200)),'   ', 200) // 200))
+// console.log((findExpression(104)),'   ', 104) // 104))
+// 
+// console.log(findExpression(7)) // undefined))
+// console.log(findExpression(63)) // undefined))
+// console.log(findExpression(23)) // undefined))
+// console.log(findExpression(103)) // undefined))
