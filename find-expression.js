8b9da27e70e3d80df9c04ebf742a4d1c3012c17d
Alann <SchnebelenAlann@hotmail.com>
2022-10-28 10:54:47 +0200

    sdqfsdf

--- find-expression.js
+++ find-expression.js
@@ -1,26 +1,43 @@
 // add4 = '+4'
 // mul2 = '*2'
 
-function reverse(arr) {
-    let newArr = new Array
-    for (let i = arr.length-1; i >= 0; i--) {
-        newArr.push(arr[i]);
-    }
-    if (typeof(arr) == 'string') {return newArr.join('')}
-    return newArr
-}
-
 
 function findExpression(nb) {
         let log = []
-        let temp = nb
-    for (; temp > 1;) {
-        // console.log(temp)
-        if (temp%2 == 0) {log.push(mul2); temp/=2} else {log.push(add4); temp-=4}
+        let temp = 1
+    
+
+    if (nb%2 == 0) {
+        log.push(mul2); temp *=2
+        for (;(nb - temp)%4 != 0;) {
+            log.push(mul2); temp *=2
+        }
+        for (let i = (nb-temp)/4; i > 0 ; i--) {
+            log.push(add4); temp += 4
+        }
+        return '1 ' + log.join('')
     }
-    log.push('1')
-    console.log(Math.trunc(temp))
-    if (1 == (temp)) {return reverse(log).join(' ')} else {return undefined}
+    if ((nb-1)%4 == 0) {
+        for (;temp != nb;) {
+            log.push(add4); temp += 4
+        }
+        return '1 ' + log.join('')
+    }
+    return undefined
 }
 
-// console.log(findExpression(144))
+console.log((findExpression(9)),'   ', 8) // 8))
+console.log((findExpression(14)),'   ', 14) // 14))
+console.log((findExpression(60)),'   ', 60) // 60))
+console.log((findExpression(100)),'   ', 100) // 100))
+console.log((findExpression(100)),'   ', 100) // 100))
+console.log((findExpression(280)),'   ', 280) // 280))
+console.log((findExpression(110)),'   ', 110) // 110))
+console.log((findExpression(144)),'   ', 144) // 144))
+console.log((findExpression(200)),'   ', 200) // 200))
+console.log((findExpression(104)),'   ', 104) // 104))
+
+console.log(findExpression(7)) // undefined))
+console.log(findExpression(63)) // undefined))
+console.log(findExpression(23)) // undefined))
+console.log(findExpression(103)) // undefined))
