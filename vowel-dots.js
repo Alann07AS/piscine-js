c8a97dcb3f5e6403e88083f094d2eb121b3c995e
Alann <SchnebelenAlann@hotmail.com>
2022-10-29 14:23:43 +0200

    hhjjbfhhrgfe

--- vowel-dots.js
+++ vowel-dots.js
@@ -1,8 +1,8 @@
+const vowels = RegExp(/[aeiouAEIOU]/, 'g')
 function vowelDots(params = '') {
     params = Array(...params)
-    const reg = RegExp(/[aeiouAEIOU]/, 'g')
     for (let i = 0; i < params.length; i++) {
-        if (reg.test(params[i])) {
+        if (vowels.test(params[i])) {
             params.splice(i+1,0,'.')
         }
     }
@@ -11,5 +11,5 @@ function vowelDots(params = '') {
 
 // const test = 'example 2, exampla 2'
 
-// console.log(test.match(reg))
+// console.log(test.match(vowels))
 // vowelDots('aeitu')
