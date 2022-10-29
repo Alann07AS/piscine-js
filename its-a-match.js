c0c0b5708982ebc48cb014e5535f9b3582f1dbb4
Alann <SchnebelenAlann@hotmail.com>
2022-10-29 11:27:29 +0200

    dfsfds

---
+++ its-a-match.js
@@ -0,0 +1,12 @@
+const normar = RegExp(/hi/)
+const begin = RegExp(/^hi/)
+const end = RegExp(/hi$/)
+const beginEnd = RegExp(/^hi$/)
+
+// const testStr = 'hi everyone, hi there'
+// 
+// const regTest = /\s*cat\s*/
+// const test = 'The fat cat sat on the mat. the concatenation'
+// 
+// console.log(test.match(regTest))
+// console.log(testStr.match(begin))
