127ee0d33ed63eb2fcc796980bf44742cd709cef
Alann <SchnebelenAlann@hotmail.com>
2022-10-29 14:48:47 +0200

    dsfdqsghlqd

--- same-amount.js
+++ same-amount.js
@@ -1,3 +1,4 @@
-function sameAmount(str, p1, p2) {
-    return p1.test(str) & p2.test(str)
+function sameAmount(str = '', p1, p2) {
+    if (str.match(RegExp(p1 ,'g')) === null || str.match(RegExp(p2 ,'g')) === null) {return false}
+    return (str.match(RegExp(p1 ,'g')).length === str.match(RegExp(p2 ,'g')).length)
 }
