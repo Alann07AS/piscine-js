89aba184aacbe3b47a3e523b16885b1fa3448799
Alann <SchnebelenAlann@hotmail.com>
2022-10-31 16:00:33 +0100

    dfsfds

--- date-is.js
+++ date-is.js
@@ -6,16 +6,16 @@ function isValid(date) {
     }
 }
 function isAfter(date, date2) {
-    return date > date2 && isValid(date) && isValid(date2)
+    return isValid(date) && isValid(date2) && date > date2
 }
 function isBefore(date, date2) {
-    return date2 > date && isValid(date) && isValid(date2)
+    return isValid(date) && isValid(date2) && date2 > date 
 }
 function isFuture(date) {
-    return Date.now() > date && isValid(date)
+    return isValid(date) && Date.now() < date
 }
 function isPast(date) {
-    return Date.now() < date  && isValid(date)
+    return Date.now() > date  && isValid(date)
 }
 
 
@@ -25,3 +25,14 @@ console.log(isValid(''));
 // console.log(isValid(new Date(1488370835081)));
 // console.log(isValid(new Date('1995-12-17T03:24:00')));
 // console.log(isValid(new Date('1995-12-17T03:24:00').getTime()));
+
+console.log( !isFuture(new Date('1992-01-01')));
+console.log( !isFuture(new Date(Date.now() - 1)));
+console.log( isFuture(new Date(2077, 11, 31)));
+console.log( isFuture(new Date(Date.now() + 1)));
+
+// isPast
+console.log( !isPast(new Date(Date.now() + 1)));
+console.log( !isPast(new Date(2077, 11, 31)));
+console.log( isPast(new Date(1442, 11, 31)));
+console.log( isPast(new Date(Date.now() - 1)));
