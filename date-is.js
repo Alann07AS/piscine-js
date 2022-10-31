225d0cf50cbef91814587a7510f799049ac2b65e
Alann <SchnebelenAlann@hotmail.com>
2022-10-31 15:48:19 +0100

    dfsfds

---
+++ date-is.js
@@ -0,0 +1,27 @@
+function isValid(date) {
+    if ( !isNaN(date) ) {
+        return true
+    } else {
+        return false
+    }
+}
+function isAfter(date, date2) {
+    return date > date2
+}
+function isBefore(date, date2) {
+    return date2 > date
+}
+function isFuture(date) {
+    return Date.now() > date
+}
+function isPast(date) {
+    return Date.now() < date
+}
+
+
+console.log(isValid("dsfsd"));
+console.log(isValid(Date.now()));
+console.log(isValid(new Date('December 17, 1995 03:24:00')));
+console.log(isValid(new Date(1488370835081)));
+console.log(isValid(new Date('1995-12-17T03:24:00')));
+console.log(isValid(new Date('1995-12-17T03:24:00').getTime()));
