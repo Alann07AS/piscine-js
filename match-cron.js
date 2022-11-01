a36e93afdd6c13f74b0b91e489dc69bfb78cca80
Alann <SchnebelenAlann@hotmail.com>
2022-11-01 14:38:14 +0100

    fdfdsf

--- match-cron.js
+++ match-cron.js
@@ -10,11 +10,21 @@ function matchCron(match ='', date) {
     if (match[2] != '*' && match[2] != date.getDate()) {
         return false
     }
-    if (match[3] != '*' && match[3] != date.getMonth()) {
+    if (match[3] != '*' && match[3]-1 != date.getMonth()) {
         return false
     }
-    if (match[4] != '*' && match[4] != date.getDay()) {
+    let day = date.getDay()
+    if (day == 0) {day = 7}
+    if (match[4] != '*' && match[4] != day) {
         return false
     }
     return true
 }
+
+console.log(matchCron('* * * * 1', new Date('2020-06-01 00:00:00')))
+console.log(matchCron('* * * 2 *', new Date('2021-02-01 00:00:00')))
+console.log(matchCron('* * 9 * *', new Date('2020-06-09 00:00:00')))
+console.log(matchCron('* 3 * * *', new Date('2020-05-31 03:00:00')))
+console.log(matchCron('1 * * * *', new Date('2020-05-30 19:01:00')))
+console.log(matchCron('3 3 * 3 3', new Date('2021-03-03 03:03:00')))
+console.log(matchCron('* * * * *', new Date()))
