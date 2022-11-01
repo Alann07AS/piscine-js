e395ce441557e2ce3d2de3fbe93fe3d8fbe9957c
Alann <SchnebelenAlann@hotmail.com>
2022-11-01 14:29:14 +0100

    fdfdsf

---
+++ match-cron.js
@@ -0,0 +1,20 @@
+function matchCron(match, date) {
+    date = new Date(date)
+    match = match.Split(' ')
+    if (match[0] != '*' && match[0] != date.getMinutes()) {
+        return false
+    }
+    if (match[1] != '*' && match[1] != date.getHours()) {
+        return false
+    }
+    if (match[2] != '*' && match[2] != date.getDate()) {
+        return false
+    }
+    if (match[3] != '*' && match[3] != date.getMonth()) {
+        return false
+    }
+    if (match[4] != '*' && match[4] != date.getDay()) {
+        return false
+    }
+    return true
+}
