0ec506207835035b9969a2a50b94e1e4490ce864
Alann <SchnebelenAlann@hotmail.com>
2022-11-02 15:06:40 +0100

    fdsfsdfds

--- using-reduce.js
+++ using-reduce.js
@@ -13,7 +13,7 @@ function sumOrMul(arr, init = 0) {
 }
 
 function funcExec(arrFunc, init = 0) {
-        arrFunc.reduce((a, b) => b(a), init)
+        return arrFunc.reduce((a, b) => b(a), init)
 }
 
 // console.log(sumOrMul([1, 2, 3, 5, 8], 5));
