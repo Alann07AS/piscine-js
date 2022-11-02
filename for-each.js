36f226c9de202ad88045a67e8c3bc9630b1a90be
Alann <SchnebelenAlann@hotmail.com>
2022-11-02 15:55:46 +0100

    dsffds

--- for-each.js
+++ for-each.js
@@ -1,5 +1,6 @@
 function forEach(arr, func) {
+    let arr2 = [...arr]
         for (let i = 0; i < arr.length; i++) {
-            func(arr[i], i, [arr[i]])
+            arr2 = func(arr[i], i, arr2)
         }
 }
