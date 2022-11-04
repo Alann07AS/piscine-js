9224b6fd02c9dc6c9dd154b10e921901884d9d16
Alann <SchnebelenAlann@hotmail.com>
2022-11-04 17:49:41 +0100

    feedqsfjndfsfds

---
+++ fifty-shades-of-cold.js
@@ -0,0 +1,26 @@
+import { colors } from "./fifty-shades-of-cold.data.js";
+
+export function generateClasses() {
+    const styleE = document.createElement("style")
+    colors.forEach(color => {
+        // styleE.setAttribute(color, 'background: '+color+';')
+        styleE.textContent += '.'+color+' { background: '+color+'; } '
+    });
+    document.head.append(styleE)
+}
+export function generateColdShades() {
+    const regCold = /(aqua|blue|turquoise|green|cyan|navy|purple)/gi
+    colors.forEach(color => {
+        // console.log(color)
+        if (regCold.test(color)) {
+            const coloDiv = document.createElement("div")
+            coloDiv.classList.add(color)
+            coloDiv.textContent = color
+            document.body.append(coloDiv)
+        }
+    });
+}
+export function choseShade(colorSelect) {
+    const allDiv = document.querySelectorAll("div")
+    allDiv.forEach((div) => {div.style.background = colorSelect})
+}
