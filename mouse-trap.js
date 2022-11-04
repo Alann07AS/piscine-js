cf8882f231a57a537421c9f5968373bd1543e53a
Alann <SchnebelenAlann@hotmail.com>
2022-11-04 23:08:50 +0100

    fdsgdsqe

---
+++ mouse-trap.js
@@ -0,0 +1,17 @@
+export function createCircle() {
+    document.addEventListener('mousedown', (e) => {
+        const circle = document.createElement("div")
+        circle.style.background = "white"
+        circle.classList.add("circle")
+        circle.style.top = e.clientY-25+"px"
+        circle.style.left = e.clientX-25+"px"
+        document.body.append(circle)
+        console.log(e, e.clientX, e.clientY);
+    })
+}
+export function moveCircle() {
+    
+}
+export function setBox() {
+    
+}
