409bb33bdad561861052b30483978a328595d51b
Alann <SchnebelenAlann@hotmail.com>
2022-11-04 14:35:08 +0100

    sdgfdfg

--- harder-bigger-bolder-stronger.js
+++ harder-bigger-bolder-stronger.js
@@ -2,13 +2,13 @@ export function generateLetters() {
     const alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
     let bold = 300
     let boldIncrease = 100
-    let boldI = (120/3)-1
+    let boldI = (120/3)
     for (let i = 0; i < 120; i++) {
         const randomLetter =  alphabet[Math.floor(Math.random() * alphabet.length)]
         const dv = document.createElement("div")
         dv.textContent = randomLetter
         dv.style.fontSize = (i+11) + "px"
-        if (i >= boldI && bold != 600) {
+        if (i >= boldI) {
             bold += boldIncrease
             boldIncrease = 200
             boldI += 40
