9074e4d17d94d93dcb9c4cfc91a9c05df7266f67
Alann <SchnebelenAlann@hotmail.com>
2022-11-05 14:21:02 +0100

    sdfhvgqdshhjdvshf

--- mouse-trap.js
+++ mouse-trap.js
@@ -19,33 +19,38 @@ export function createCircle() {
 }
 export function moveCircle() {
     document.addEventListener('mousemove', (e) => {
-        console.log('AAAAAAAAAAAAAAAAA')
         const lastChild = document.body.lastChild
+
         if (lastChild.classList.contains('circle')) {
-        if (!isTrap) {
-            lastChild.style.background = "white"
-            lastChild.style.left = e.clientX-25+"px"
-            lastChild.style.top = e.clientY-25+"px"
-        } else {
-            lastChild.style.background = "var(--purple)"
-            if (e.clientX >  boxData.x+26 && e.clientX < boxData.width+boxData.x-26) {
-                lastChild.style.left = e.clientX-26+"px"
-            } else if (e.clientX <  boxData.x+26){
-                lastChild.style.left = boxData.x+"px"
+            if ((e.clientX >  boxData.x-25 && e.clientX < boxData.width+boxData.x+25 && e.clientY >  boxData.y-25 && e.clientY < boxData.height+boxData.y+25)) {
+                console.log('AAAAAAAAAAAAAAA')
+                lastChild.style.background = "var(--purple)"
             } else {
-                lastChild.style.left = boxData.width+boxData.x-51+"px"
+                lastChild.style.background = "white"
             }
-            if (e.clientY >  boxData.y+26 && e.clientY < boxData.height+boxData.y-26) {
-                lastChild.style.top = e.clientY-26+"px"
-            } else if(e.clientY <  boxData.y+26) {
-                lastChild.style.top = boxData.y+"px"
+            if (!isTrap) {
+                lastChild.style.left = e.clientX-25+"px"
+                lastChild.style.top = e.clientY-25+"px"
             } else {
-                lastChild.style.top = boxData.height+boxData.y-51+"px"
+                lastChild.style.background = "var(--purple)"
+                if (e.clientX >  boxData.x+26 && e.clientX < boxData.width+boxData.x-26) {
+                    lastChild.style.left = e.clientX-26+"px"
+                } else if (e.clientX <  boxData.x+26){
+                    lastChild.style.left = boxData.x+"px"
+                } else {
+                    lastChild.style.left = boxData.width+boxData.x-51+"px"
+                }
+                if (e.clientY >  boxData.y+26 && e.clientY < boxData.height+boxData.y-26) {
+                    lastChild.style.top = e.clientY-26+"px"
+                } else if(e.clientY <  boxData.y+26) {
+                    lastChild.style.top = boxData.y+"px"
+                } else {
+                    lastChild.style.top = boxData.height+boxData.y-51+"px"
+                }
             }
-        }
-        if ((e.clientX >  boxData.x+25 && e.clientX < boxData.width+boxData.x-25 && e.clientY >  boxData.y+25 && e.clientY < boxData.height+boxData.y-25)) {
-            isTrap = true
-        }
+            if ((e.clientX >  boxData.x+25 && e.clientX < boxData.width+boxData.x-25 && e.clientY >  boxData.y+25 && e.clientY < boxData.height+boxData.y-25)) {
+                isTrap = true
+            }
         }
     })
 }
@@ -56,9 +61,12 @@ export function setBox() {
     box.style.justifyContent = 'center'
     box.style.alignContent = 'center'
     document.body.append(box)
-    // box.addEventListener("mou", (e) => {
-    //     console.log('mouse enter')
-    //     isTrap = true
-    // })
+    box.addEventListener('mouseenter', (e) => {
+        console.log('mouse over')
+        const lastChild = document.body.lastChild
+        if (lastChild.classList.contains('circle')) {
+            lastChild.style.background = "var(--purple)"
+        }
+    })
     boxData = document.querySelector("box").getBoundingClientRect()
 }
