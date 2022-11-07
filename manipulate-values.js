bd2b88a4ec9fe3bcb5fd4ec0e7fd0629c1fce130
Alann <SchnebelenAlann@hotmail.com>
2022-11-07 15:26:50 +0100

    sdfhbdsfsd

---
+++ manipulate-values.js
@@ -0,0 +1,23 @@
+function filterValues(obj, func) {
+    const newObj = {}
+    for (const [key, value] of Object.entries(obj)) {
+        if (func(value)) {
+            newObj[key] = value
+        }
+    }
+    return newObj
+}
+function mapValues(obj, func) {
+    const newObj = {}
+    for (const [key, value] of Object.entries(obj)) {
+        newObj[key] = (func(value))
+    }
+    return newObj
+}
+function reduceValues(obj, func) {
+    let acc = obj[Object.keys(obj)[0]]
+    for (const [key, value] of Object.entries(obj)) {
+        acc = (func(acc, value))
+    }
+    return acc
+}
