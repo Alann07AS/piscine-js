d3cdadbde330596487e5ebaf132f037b7d73341d
Alann <SchnebelenAlann@hotmail.com>
2022-11-09 13:06:06 +0100

    efkdvdshfhjerhj

--- curry-entries.js
+++ curry-entries.js
@@ -15,13 +15,13 @@ function defaultCurry(obj1) {
 // })
 // );
 
-const personnel = {
-    lukeSkywalker: { id: 5,  pilotingScore: 98, shootingScore: 56, isForceUser: true  },
-    sabineWren:    { id: 82, pilotingScore: 73, shootingScore: 99, isForceUser: false },
-    zebOrellios:   { id: 22, pilotingScore: 20, shootingScore: 59, isForceUser: false },
-    ezraBridger:   { id: 15, pilotingScore: 43, shootingScore: 67, isForceUser: true  },
-    calebDume:     { id: 11, pilotingScore: 71, shootingScore: 85, isForceUser: true  },
-}
+    // const personnel = {
+    //     lukeSkywalker: { id: 5,  pilotingScore: 98, shootingScore: 56, isForceUser: true  },
+    //     sabineWren:    { id: 82, pilotingScore: 73, shootingScore: 99, isForceUser: false },
+    //     zebOrellios:   { id: 22, pilotingScore: 20, shootingScore: 59, isForceUser: false },
+    //     ezraBridger:   { id: 15, pilotingScore: 43, shootingScore: 67, isForceUser: true  },
+    //     calebDume:     { id: 11, pilotingScore: 71, shootingScore: 85, isForceUser: true  },
+    // }
 
 function mapCurry(func) {
     return function (obj) {
